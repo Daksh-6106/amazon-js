daf3f99046db09ba03d55dec4767dcb45212c437
Daksh-6106 <guptadaksh130@gmail.com>
2025-02-09 17:17:27 +0530

    added all the products

--- scripts/home.js
+++ scripts/home.js
@@ -1,29 +1,3 @@
-const products = [{
-    name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    image: 'images/athletic-cotton-socks-6-pairs.jpg',
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    price: 666,
-}, {
-    name: 'Basketball Size-7',
-    image: 'images/intermediate-composite-basketball.jpg',
-    rating: {
-        stars: 4,
-        count: 127,
-    },
-    price: 800
-}, {
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    image: 'images/adults-plain-cotton-tshirt-2-pack.jpg',
-    rating: {
-        stars: 4.5,
-        count: 95,
-    },
-    price: 500
-}];
-
 let productsHTML = '';
 
 products.forEach((product) => {
@@ -34,7 +8,7 @@ products.forEach((product) => {
                 </div>
                 <div class="product-name  limit-to-2-lines">${product.name}</div>
                 <div class="product-rating">
-                    <img class="product-rating-stars" src="images/rating-${product.rating.stars * 10}.png">
+                    <img class="product-rating-stars" src="images/rating/rating-${product.rating.stars * 10}.png">
                     <div class="product-rating-count">${product.rating.count}</div>
                 </div>
                 <div class="product-price">₹${product.price}</div>
